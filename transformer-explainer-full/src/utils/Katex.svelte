51d3ec355618f0cc76c01320c2904aa26ce6ecaa
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2026-01-22 17:22:35 +0800

    transformer explainer full

---
+++ transformer-explainer-full/src/utils/Katex.svelte
@@ -0,0 +1,25 @@
+<script>
+	import katex from 'katex';
+
+	export let math;
+	export let displayMode = false;
+	export let style;
+
+	const options = {
+		displayMode,
+		throwOnError: false,
+		style
+	};
+
+	$: katexString = katex.renderToString(math, options);
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="/katex/katex.min.css"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
+
+{@html katexString}
